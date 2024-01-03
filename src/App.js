0713fb6db56a245b89e69b6528d1483cad5ce61e
Ahmed Heggy <ahmedheggy@pursuit.org>
2024-01-02 19:20:33 -0500

    adding functionality to the buttons

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
+  const addOne = () => {
+    setCount(prevCount => prevCount + 1);
+  }
+
+
+  const multiplyByTwo = () => {
+    setCount(count * 2)
+  }
+
+
   return (
     <div className="App">
       <div className="number">{count}</div>
       <div className="buttons-container">
-        <div className="counter-button">+1</div>
-        <div className="counter-button blue">x2</div>
+        <div className="counter-button" onClick={addOne}>+1</div>
+        <div className="counter-button blue" onClick={multiplyByTwo}>x2</div>
       </div>
     </div>
   );
